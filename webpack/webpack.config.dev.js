cf1f4ea03dc899ea0deb99e04817b6c6ac1c77c2
Hannu-Pekka Heinäjärvi <hannu-pekka.heinajarvi@futurice.com>
2016-08-19 09:34:57 +0300

    Fix sourceMaps on dev.

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -33,7 +33,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: 'style!css!postcss'
+        loaders: [
+          'style?sourceMap',
+          'css?importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+          'postcss',
+        ]
       },
       {
         test: /\.less$/,
@@ -41,7 +45,7 @@ module.exports = {
           'style?sourceMap',
           'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
           'postcss',
-          'less'
+          'less?sourceMap'
         ]
       },
       {
