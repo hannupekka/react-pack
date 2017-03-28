347df2a73c3ef2b8121040ae48d961145d616027
Hannu-Pekka Heinäjärvi <hannu-pekka.heinajarvi@futurice.com>
2017-03-28 16:59:46 +0300

    Fix wrong path.

--- src/utils/history.js
+++ src/utils/history.js
@@ -1,6 +1,6 @@
 // @flow
 import { useRouterHistory } from 'react-router';
-import createHistory from 'history/createBrowserHistory';
+import createHistory from 'history/lib/createBrowserHistory';
 
 let historyInstance: ?Object = null;
 
